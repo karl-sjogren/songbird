710d299d4eac93e3f33379419c2fc68dd3340c9d
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2021-02-27 22:59:01 +0100

    Minified css painter

--- src/Songbird.Frontend/public/css-painters/rough-boxes.js
+++ src/Songbird.Frontend/public/css-painters/rough-boxes.js
@@ -3,135 +3,9 @@
  * Painter taken from https://css-houdini.rocks/posts/rough-boxes/paint.js
  * with more information available at https://css-houdini.rocks/rough-boxes
  */
-class RoughDrawable {
-  constructor() {
-    this._fields = {};
-    this._dirty = false;
-    this._canvas = null;
-    this.z = 0;
-    this.roughness = 0;
-    this.bowing = 1;
-    this._stroke = null;
-    this._strokeWidth = null;
-
-    this._fill = null;
-    this._fillStyle = null;
-    this._fillWeight = null;
-    this._hachureAngle = null;
-    this._hachureGap = null;
-
-    this.maxRandomnessOffset = 1;
-    this._curveTightness = 0;
-  }
-
-  getOffset(min, max) {
-    return this.roughness * ((Math.random() * (max - min)) + min);
-  }
-
-  drawLine(ctx, x1, y1, x2, y2, existingPath) {
-    let lengthSq = Math.pow((x1 - x2), 2) + Math.pow((x1 - x2), 2);
-    let offset = this.maxRandomnessOffset || 0;
-    if(offset * offset * 100 > lengthSq) {
-      offset = Math.sqrt(lengthSq) / 10;
-    }
-    let halfOffset = offset / 2;
-    let divergePoint = 0.2 + Math.random() * 0.2;
-    // Midpoint displacement value to give slightly bowed lines.
-    let midDispX = this.bowing * this.maxRandomnessOffset * (y2 - y1) / 200;
-    let midDispY = this.bowing * this.maxRandomnessOffset * (x1 - x2) / 200;
-    midDispX = this.getOffset(-midDispX, midDispX);
-    midDispY = this.getOffset(-midDispY, midDispY);
-
-    if(!existingPath) {
-      ctx.beginPath();
-    }
-    ctx.moveTo(x1 + this.getOffset(-offset, offset), y1 + this.getOffset(-offset, offset));
-    ctx.bezierCurveTo(midDispX + x1 + (x2 - x1) * divergePoint + this.getOffset(-offset, offset),
-      midDispY + y1 + (y2 - y1) * divergePoint + this.getOffset(-offset, offset),
-      midDispX + x1 + 2 * (x2 - x1) * divergePoint + this.getOffset(-offset, offset),
-      midDispY + y1 + 2 * (y2 - y1) * divergePoint + this.getOffset(-offset, offset),
-      x2 + this.getOffset(-offset, offset),
-      y2 + this.getOffset(-offset, offset));
-    if(!existingPath) {
-      ctx.stroke();
-    }
-    if(!existingPath) {
-      ctx.beginPath();
-    }
-    ctx.moveTo(x1 + this.getOffset(-halfOffset, halfOffset), y1 + this.getOffset(-halfOffset, halfOffset));
-    ctx.bezierCurveTo(midDispX + x1 + (x2 - x1) * divergePoint + this.getOffset(-halfOffset, halfOffset),
-      midDispY + y1 + (y2 - y1) * divergePoint + this.getOffset(-halfOffset, halfOffset),
-      midDispX + x1 + 2 * (x2 - x1) * divergePoint + this.getOffset(-halfOffset, halfOffset),
-      midDispY + y1 + 2 * (y2 - y1) * divergePoint + this.getOffset(-halfOffset, halfOffset),
-      x2 + this.getOffset(-halfOffset, halfOffset),
-      y2 + this.getOffset(-halfOffset, halfOffset));
-    if(!existingPath) {
-      ctx.stroke();
-    }
-  }
-}
-class RoughRectangle extends RoughDrawable {
-  constructor(x, y, width, height) {
-    super(['x', 'y', 'width', 'height']);
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
-
-  draw(ctx) {
-    let left = this.x;
-    let right = this.x + this.width;
-    let top = this.y;
-    let bottom = this.y + this.height;
-
-    if(this.fill) {
-      this._doFill(ctx, left, right, top, bottom);
-    }
-
-    ctx.save();
-    ctx.strokeStyle = this.stroke;
-    ctx.lineWidth = this.strokeWidth;
-    this.drawLine(ctx, left, top, right, top);
-    this.drawLine(ctx, right, top, right, bottom);
-    this.drawLine(ctx, right, bottom, left, bottom);
-    this.drawLine(ctx, left, bottom, left, top);
-    ctx.restore();
-  }
-  _doFill(ctx, left, right, top, bottom) {
-    var fillStyle = this.fillStyle || 'hachure';
-    switch (fillStyle) {
-      case 'solid': {
-        ctx.save();
-        ctx.fillStyle = this.fill;
-        let o = this.maxRandomnessOffset || 0;
-        var points = [
-          [left + this.getOffset(-o, o), top + this.getOffset(-o, o)],
-          [right + this.getOffset(-o, o), top + this.getOffset(-o, o)],
-          [right + this.getOffset(-o, o), bottom + this.getOffset(-o, o)],
-          [left + this.getOffset(-o, o), bottom + this.getOffset(-o, o)]
-        ];
-        ctx.beginPath();
-        ctx.moveTo(points[0][0], points[0][1]);
-        ctx.lineTo(points[1][0], points[1][1]);
-        ctx.lineTo(points[2][0], points[2][1]);
-        ctx.lineTo(points[3][0], points[3][1]);
-        ctx.fill();
-        ctx.restore();
-        break;
-      }
-      default: {
-        let xc = [left, right, right, left];
-        let yc = [top, top, bottom, bottom];
-        this.hachureFillShape(ctx, xc, yc);
-        break;
-      }
-    }
-  }
-}
+class RoughRectangle extends class{constructor(){this._fields={},this._dirty=!1,this._canvas=null,this.z=0,this.roughness=0,this.bowing=1,this._stroke=null,this._strokeWidth=null,this._fill=null,this._fillStyle=null,this._fillWeight=null,this._hachureAngle=null,this._hachureGap=null,this.maxRandomnessOffset=1,this._curveTightness=0}getOffset(t,s){return this.roughness*(Math.random()*(s-t)+t)}drawLine(t,s,e,i,h,f){let l=Math.pow(s-i,2)+Math.pow(s-i,2),n=this.maxRandomnessOffset||0;n*n*100>l&&(n=Math.sqrt(l)/10);let g=n/2,o=.2+.2*Math.random(),r=this.bowing*this.maxRandomnessOffset*(h-e)/200,a=this.bowing*this.maxRandomnessOffset*(s-i)/200;r=this.getOffset(-r,r),a=this.getOffset(-a,a),f||t.beginPath(),t.moveTo(s+this.getOffset(-n,n),e+this.getOffset(-n,n)),t.bezierCurveTo(r+s+(i-s)*o+this.getOffset(-n,n),a+e+(h-e)*o+this.getOffset(-n,n),r+s+2*(i-s)*o+this.getOffset(-n,n),a+e+2*(h-e)*o+this.getOffset(-n,n),i+this.getOffset(-n,n),h+this.getOffset(-n,n)),f||t.stroke(),f||t.beginPath(),t.moveTo(s+this.getOffset(-g,g),e+this.getOffset(-g,g)),t.bezierCurveTo(r+s+(i-s)*o+this.getOffset(-g,g),a+e+(h-e)*o+this.getOffset(-g,g),r+s+2*(i-s)*o+this.getOffset(-g,g),a+e+2*(h-e)*o+this.getOffset(-g,g),i+this.getOffset(-g,g),h+this.getOffset(-g,g)),f||t.stroke()}}{constructor(t,s,e,i){super(["x","y","width","height"]),this.x=t,this.y=s,this.width=e,this.height=i}draw(t){let s=this.x,e=this.x+this.width,i=this.y,h=this.y+this.height;this.fill&&this._doFill(t,s,e,i,h),t.save(),t.strokeStyle=this.stroke,t.lineWidth=this.strokeWidth,this.drawLine(t,s,i,e,i),this.drawLine(t,e,i,e,h),this.drawLine(t,e,h,s,h),this.drawLine(t,s,h,s,i),t.restore()}_doFill(t,s,e,i,h){t.save(),t.fillStyle=this.fill;let f=this.maxRandomnessOffset||0;var l=[[s+this.getOffset(-f,f),i+this.getOffset(-f,f)],[e+this.getOffset(-f,f),i+this.getOffset(-f,f)],[e+this.getOffset(-f,f),h+this.getOffset(-f,f)],[s+this.getOffset(-f,f),h+this.getOffset(-f,f)]];t.beginPath(),t.moveTo(l[0][0],l[0][1]),t.lineTo(l[1][0],l[1][1]),t.lineTo(l[2][0],l[2][1]),t.lineTo(l[3][0],l[3][1]),t.fill(),t.restore()}}; /* eslint-disable-line */
 
 registerPaint('rough-boxes', class {
-
   static get inputProperties() {
     return [
       '--rough-fill',
@@ -145,7 +19,6 @@ registerPaint('rough-boxes', class {
     const padding = 20;
     var rect = new RoughRectangle(padding, padding, geom.width - padding * 2, geom.height - padding * 2);
     rect.roughness = properties.get('--rough-roughness').toString();
-    rect.fillStyle = 'solid';
     rect.fill = properties.get('--rough-fill').toString();
     rect.stroke = properties.get('--rough-stroke').toString();
     rect.strokeWidth = properties.get('--rough-stroke-width').toString().replace('px', '');
