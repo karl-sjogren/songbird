c48aac5c20743970f37f0e5a6f4a5e3d95432eba
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2021-03-23 23:27:07 +0100

    Updated ember test reporting

--- src/Songbird.Frontend/testem.js
+++ src/Songbird.Frontend/testem.js
@@ -1,11 +1,32 @@
-'use strict';
+const fs = require('fs');
+const MultiReporter = require('testem-multi-reporter');
+const GitLabReporter = require('testem-gitlab-reporter');
+// eslint-disable-next-line node/no-extraneous-require
+const TAPReporter = require('testem/lib/reporters/tap_reporter');
+
+let reporter = new MultiReporter({
+  reporters: [
+    {
+      ReporterClass: TAPReporter,
+      args: [false, null, { get: () => false }],
+    },
+    {
+      ReporterClass: GitLabReporter,
+      args: [false, fs.createWriteStream('junit.xml'), { get: () => false }],
+    },
+  ],
+});
 
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
-  browser_start_timeout: 120,
+  reporter: reporter,
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
   browser_args: {
     Chrome: {
       ci: [
@@ -16,8 +37,8 @@ module.exports = {
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
-  },
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
