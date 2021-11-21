a1ede664093bdd49f7bb31be07a49fd056d35b46
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2021-11-21 11:13:10 +0100

    Updated Ember to latest stable

--- src/Songbird.Frontend/config/targets.js
+++ src/Songbird.Frontend/config/targets.js
@@ -6,6 +6,7 @@ const browsers = [
   //'last 1 Safari versions', // NOPE!
 ];
 
+<<<<<<< HEAD
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
 
@@ -13,6 +14,22 @@ if(isCI || isProduction) {
   // Lets ignore IE11 since this is an internal tool
   //browsers.push('ie 11');
 }
+=======
+// Ember's browser support policy is changing, and IE11 support will end in
+// v4.0 onwards.
+//
+// See https://deprecations.emberjs.com/v3.x#toc_3-0-browser-support-policy
+//
+// If you need IE11 support on a version of Ember that still offers support
+// for it, uncomment the code block below.
+//
+// const isCI = Boolean(process.env.CI);
+// const isProduction = process.env.EMBER_ENV === 'production';
+//
+// if (isCI || isProduction) {
+//   browsers.push('ie 11');
+// }
+>>>>>>> d42d978 (v3.25.0...v3.28.4)
 
 module.exports = {
   browsers,
