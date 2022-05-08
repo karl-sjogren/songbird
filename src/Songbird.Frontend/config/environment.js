8cf5e48b2f696c9a66ba39bfd44ff7b92880158c
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2022-05-08 22:23:56 +0200

    Updated frontend dependencies

--- src/Songbird.Frontend/config/environment.js
+++ src/Songbird.Frontend/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'songbird',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
